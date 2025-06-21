a85d4d55ac075c25ffd9a212888c44f8ecffe55c
Raphael <lurgoyf82@hotmail.com>
2025-06-21 08:13:37 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
-declare(strict_types=1);
-
-    require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../src/autoloader.php';
 
     use src\Entities\Cart;
     use src\Services\InputParser;
